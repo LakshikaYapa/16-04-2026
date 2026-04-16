4cfa3c069d6ce0759510861673d1ad037fe5856c
LakshikaYapa <lakshikayapa2@gmail.com>
2026-04-16 20:50:01 +0530

    Initial project setuped with Vue 3, TypeScript, Tailwind

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-red-100 text-black text-3xl p-10">
+    TEST TAILWIND
+  </div>
+</template>
